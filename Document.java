a9281e3cdb7bd31c1f69f881712d84e8a253c886
DESKTOP-5GAN8AK\gabih <gabihartobanu@email.com>
2018-03-22 14:43:47 +0200

    na de aci

---
+++ Document.java
@@ -0,0 +1,27 @@
+import java.util.Vector;
+
+public class Document {
+
+  public string buffer;
+
+  public String tempbuffer;
+
+  public String language;
+
+  public Integer cursorPosition;
+
+  public String path;
+
+    public Vector  myIDE;
+
+  public void moveCursor() {
+  }
+
+  public char addCharacter() {
+  return 'x';
+  }
+
+  public void Save() {
+  }
+
+}
---
+++ IDE.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class IDE {
+
+  public List<Document> document;
+
+    /**
+   * 
+   * @element-type Document
+   */
+  public Vector  myDocument;
+
+  public void OpenDocument() {
+  }
+
+  public void ChangeFocus( doc) {
+  }
+
+  public void saveAll() {
+  }
+
+}
