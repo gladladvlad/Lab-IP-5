b1f5bb815dc9c9992defefaabb8efce072f1ddda
Fr33Vlad <vlady472@yahoo.com>
2018-03-22 15:19:23 +0200

    am pus

--- IP/src/ip/Document.java
+++ IP/src/ip/Document.java
@@ -3,18 +3,27 @@ import java.util.Vector;
 public class Document {
 
   public String buffer;
-
   public String tempbuffer;
-
   public String language;
-
   public Integer cursorPosition;
-
   public String path;
 
-    public Vector  myIDE;
+    public Document() {
+        
+    }
+
+    public Document(String path) {
+        this.path = path;
+        tempbuffer = "";
+        language = "Default";
+        cursorPosition = 0;
+        System.out.println("Am instantiat un document!");
+    }
+    
+  
 
   public void moveCursor(int offs) {
+      cursorPosition = cursorPosition + offs;
   }
 
   public void addCharacter() {
